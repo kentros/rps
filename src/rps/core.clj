ad2820cfbeb7617f16df1283bd57553e443864cf
Kenny Ingle <kentron@gmail.com>
2014-07-24 11:12:56 -0500

    first commit

---
+++ src/rps/core.clj
@@ -0,0 +1,58 @@
+(ns rps.core
+  (:require [clojure.data.generators :refer [weighted]])
+  (:import jline.Terminal)
+  (:gen-class))
+
+(def what-beats {:rock :paper, :paper :scissors, :scissors :rock})
+
+(defn winner [p1 p2]
+  (cond
+    (or
+      (and (= p1 :rock) (= p2 :scissors))
+      (and (= p1 :paper) (= p2 :rock)) 
+      (and (= p1 :scissors) (= p2 :paper))) p1
+    (or
+      (and (= p2 :rock) (= p1 :scissors))
+      (and (= p2 :paper) (= p1 :rock)) 
+      (and (= p2 :scissors) (= p1 :paper))) p2
+    :else nil))
+
+(defn battle [human comp]
+  (let [h (name human)
+        c (name comp)]
+    (cond
+      (= human comp) (println (format "We both picked %s. DRAW!" c))
+      (= human (winner human comp))
+        (println (format "Your %s defeats computer's %s. YOU WIN!" h c))
+      (= comp (winner human comp))
+        (println (format "Computer's %s defeats your %s. YOU LOSE!" c h))
+      :else (println (format "Wat? %s and %s ?" h c)))))
+
+(defn key->rps [k]
+  (cond
+    (or (= k 82) (= k 114)) :rock
+    (or (= k 80) (= k 112)) :paper
+    (or (= k 83) (= k 115)) :scissors
+    :else nil))
+
+(defn play-game [freqs]
+    (println "\n(R)ock, (P)aper, (S)cissors?")
+    (let [term (Terminal/getTerminal)
+          rps (key->rps (.readCharacter term System/in))]
+      (if-not (nil? rps)
+        (do 
+          (battle rps 
+(what-beats (weighted freqs))
+)
+
+
+ 
+          (recur (assoc freqs rps (inc (rps freqs)))))
+        (println "Game Over Man!  Game Over!"))))
+  
+
+(defn -main
+  "Rock, Paper, Scissors!"
+  [& args]
+  (play-game {:rock 1, :paper 1, :scissors 1}))
+  
